337dfa9e921d6621327fbcb358174e1177044cc3
Stanislav Smirnov <stanislav.ktak.smirnov@gmail.com>
2022-05-04 15:42:56 +0700

    Visual improvement: inactive mode-line now has the same font as the active one

--- personal.el
+++ personal.el
@@ -47,6 +47,7 @@
 ;; Some fonts
 (set-face-font 'default "Fira Code-14")
 (set-face-font 'mode-line "Fira Code-10")
+(set-face-font 'mode-line-inactive "Fira Code-10")
 (eval-after-load "linum"
   '(set-face-attribute 'linum nil :height 100))
 
